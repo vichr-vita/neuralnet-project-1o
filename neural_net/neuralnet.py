89cdc7df217da1181cc6ef38c2d58d1597bb9acb
Vit Chrubasik <flow3rish@protonmail.com>
2020-11-22 02:26:37 +0100

    implement cost gradient (backpropagation)

--- neural_net/neuralnet.py
+++ neural_net/neuralnet.py
@@ -41,10 +41,18 @@ class NeuralNet:
     @staticmethod
     def sigmoid_inv(sig: np.ndarray) -> np.ndarray:
         """
-        returns z that has been transformed through sigmoid func
+        return z that has been transformed through sigmoid func
 
         loses precision for values close to 0 and 1
         """
+        return np.log(sig / (1 - sig))
+
+    @staticmethod
+    def sigmoid_prime(z):
+        """
+        return first derivative of the sigmoid function with respect to z
+        """
+        return NeuralNet.sigmoid(z)*(1-NeuralNet.sigmoid(z))
 
     @staticmethod
     def z_func(a: np.ndarray, w: np.ndarray, b: np.ndarray) -> np.ndarray:
@@ -63,14 +71,20 @@ class NeuralNet:
     def feed_forward(self, input_vec: np.ndarray) -> np.ndarray:
         """
         return a result as an output vector of shape (self.layer_sizes[-1],)
+        save the state of activations and corresponding zs
         """
         if self.layer_sizes[0] != len(input_vec):
             raise NeuralNetException(f'input vector size ({len(input_vec)}) does not match input layer size ({self.layer_sizes[0]})')
 
         a = input_vec
+        self.a_list = [a]
+        self.z_list = []
         for w, b in zip(self.weights, self.biases):
-            a = NeuralNet.sigmoid(NeuralNet.z_func(a, w, b))
-        return a
+            z = NeuralNet.z_func(a, w, b)
+            self.z_list.append(z)
+            a = NeuralNet.sigmoid(z)
+            self.a_list.append(a)
+        return a # final output layer
 
     @staticmethod
     def cost_feed(output_vec: np.ndarray, desired_output_vec: np.ndarray) -> float:
@@ -83,3 +97,34 @@ class NeuralNet:
         shapes of the vectors are to be the same
         """
         return np.sum(np.power(output_vec - desired_output_vec, 2))
+
+    def get_cost_gradient(self, input_vec: np.ndarray, desired_output_vec: np.ndarray):
+        w_grad = [np.zeros(w.shape) for w in self.weights]
+        b_grad = [np.zeros(b.shape) for b in self.biases]
+
+        a = input_vec
+        # a_list = [a]
+        # z_list = []
+        # for w, b in zip(self.weights, self.biases):
+        #     z = NeuralNet.z_func(a, w, b)
+        #     z_list.append(z)
+        #     a = NeuralNet.sigmoid(z)
+        #     a_list.append(a)
+
+        self.feed_forward(a)
+        
+        delta = self.cost_partial_derivative_to_a(self.a_list[-1], desired_output_vec) * NeuralNet.sigmoid_prime(self.z_list[-1])
+        w_grad[-1] = np.dot(delta[..., None], self.a_list[-2][None, ...])
+        b_grad[-1] = delta
+
+        for i in range(2, len(self.layer_sizes)):
+            z = self.z_list[-i]
+            delta = np.dot(self.weights[-i+1].T, delta) * NeuralNet.sigmoid_prime(z)
+            w_grad[-i] = np.dot(delta[..., None], self.a_list[-i-1][None, ...])
+            b_grad[-i] = delta
+        return (w_grad, b_grad)
+
+    
+    @staticmethod
+    def cost_partial_derivative_to_a(output_vec, desired_output_vec):
+        return 2*(output_vec - desired_output_vec)
--- test/test_neuralnet.py
+++ test/test_neuralnet.py
@@ -6,36 +6,59 @@ from neural_net.neuralnet import NeuralNet, NeuralNetException
 class NeuralNetTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.x = np.linspace(-4, 4, 1000)
-        self.cosh = np.cosh(self.x)
+        pass
 
+
+    def test_basic(self):
+        x = np.linspace(-4, 4, 1000)
+        cosh = np.cosh(x)
         # make inputs the list of numpy arrays
-        self.sig_x = [np.array([NeuralNet.sigmoid(n)]) for n in self.x]
-        self.sig_cosh = [np.array([NeuralNet.sigmoid(n)]) for n in self.cosh]
+        sig_x = [np.array([NeuralNet.sigmoid(n)]) for n in x]
+        sig_cosh = [np.array([NeuralNet.sigmoid(n)]) for n in cosh]
 
-    def test_neuralnet(self):
-        nn = NeuralNet(layer_sizes=[len(self.sig_x[0]), 3, 4, 1], initialize=False) # first layer created to match the input size
-        print(nn.layer_sizes)
+        nn = NeuralNet(layer_sizes=[len(sig_x[0]), 3, 4, 1], initialize=False) # first layer created to match the input size
         self.assertTrue(len(nn.biases) == len(nn.layer_sizes) - 1)
         for b, s in zip(nn.biases, nn.layer_sizes[1:]):
             self.assertEqual(len(b), s)
         nn.initialize_weights()
         nn.initialize_biases()
+        # print(nn.weights)
 
         wrong_type = 3
         self.assertRaises(TypeError, NeuralNet.z_func, wrong_type, nn.weights[0], nn.biases[0])
         
         layer_no = 1 # second layer
-        training_data_index = np.random.randint(0, len(self.sig_x)) # random input from the data set
+        training_data_index = np.random.randint(0, len(sig_x)) # random input from the data set
 
-        z = NeuralNet.z_func(self.sig_x[training_data_index], nn.weights[layer_no - 1], nn.biases[layer_no - 1])
+        z = NeuralNet.z_func(sig_x[training_data_index], nn.weights[layer_no - 1], nn.biases[layer_no - 1])
         self.assertEqual(len(z.shape), 1) # check if returns a vector, not a matrix
         self.assertEqual(z.shape[0], nn.layer_sizes[layer_no]) # check shape of the vector agains the layer
         self.assertTrue([0 <= x <= 1 for x in NeuralNet.sigmoid(z)]) # check sigmoid implementation
 
-        outputs = nn.feed_forward(self.sig_x[training_data_index])
+        outputs = nn.feed_forward(sig_x[training_data_index])
         self.assertEqual(len(outputs.shape), 1) # check if returns a vector, not a matrix
         self.assertEqual(outputs.shape[0], nn.layer_sizes[-1]) # check shape of the vector against the layer
 
-        cost = NeuralNet.cost_feed(outputs, self.sig_cosh[training_data_index])
-        print(cost)
+        cost = NeuralNet.cost_feed(outputs, sig_cosh[training_data_index])
+        # print(cost)
+
+    def test_backpropagation(self):
+        x = np.linspace(-4, 4, 1000)
+        cosh = np.cosh(x)
+        # make inputs the list of numpy arrays
+        sig_x = [np.array([NeuralNet.sigmoid(n), NeuralNet.sigmoid(n**2)]) for n in x]
+        sig_cosh = [np.array([NeuralNet.sigmoid(n), NeuralNet.sigmoid(n**2)]) for n in cosh]
+        nn = NeuralNet(layer_sizes=[len(sig_x[0]), 3, 4, len(sig_cosh[0])]) # first layer created to match the input size
+        training_data_index = np.random.randint(0, len(sig_x)) # random input from the data set
+        grad_weights, grad_biases = nn.get_cost_gradient(sig_x[training_data_index], sig_cosh[training_data_index])
+
+        for gw, w in zip(grad_weights, nn.weights):
+            self.assertEqual(gw.shape, w.shape)
+        for gb, b in zip(grad_biases, nn.biases):
+            self.assertEqual(gb.shape, b.shape)
+    
+    def test_network_wb_update(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
